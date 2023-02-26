ad3240a15060321792106224d742942ad8bf4200
skais <85506814+skaisx@users.noreply.github.com>
2023-02-26 14:20:51 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 enter your license key:
 
-if successful, then loadstring:(source.lua)
+if successful, then loadstring(game:HttpGet("https://raw.githubusercontent.com/skaisx/void/main/source.lua", true))()
